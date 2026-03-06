a999c361eb9a7f09710f85c6ab9f99a94f7bca2a
Rishitha73 <rishithakummara@gmail.com>
2026-03-06 12:09:41 +0530

    Fix Express options wildcard error

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -43,9 +43,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors(corsOptions));
 
-// Handle preflight requests
-app.options('*', cors(corsOptions));
-
 // Route files
 const authRoutes = require('../../routes/authRoutes');
 const triageRoutes = require('../../routes/triageRoutes');
